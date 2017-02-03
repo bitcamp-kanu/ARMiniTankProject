755ae58db717afe20784758bf07e3e7cd0b4894e
taeng <taengoov@gmail.com>
2017-02-03 21:34:47 +0900

    2차 수정본 백동현

--- TankAttack Templet/TankAttack Templet/Assets/02.Scripts/FireCannon.cs
+++ TankAttack Templet/TankAttack Templet/Assets/02.Scripts/FireCannon.cs
@@ -10,9 +10,21 @@ public class FireCannon : MonoBehaviour {
     //cannon 발사 지점
     public Transform firePos;
 
+    //포탄 발사 사운드 파일
+    private AudioClip fireSfx = null;
+
+    //AudioSource 컴포넌트를 할당할 변수
+    private AudioSource sfx = null;
+
 	void Awake () {
         //cannon 프리팹을 Resources 폴더에서 불러와 변수에 할당
         cannon = (GameObject)Resources.Load("Cannon");
+
+        //포탄 발사 사운드 파일을 Resources 폴더에서 불러와 변수에 할당
+        fireSfx = Resources.Load<AudioClip>("Cannonfire");
+
+        //AudioSource 컴포넌트를 할당
+        sfx = GetComponent<AudioSource>();
 	}
 	
 	
@@ -25,5 +37,8 @@ public class FireCannon : MonoBehaviour {
     void Fire()
     {
         Instantiate(cannon, firePos.position, firePos.rotation);
+
+        //발사 사운드 발생
+        sfx.PlayOneShot(fireSfx, 2.0f);
     }
 }
--- TankAttack Templet/TankAttack Templet/Assets/02.Scripts/TankMove.cs
+++ TankAttack Templet/TankAttack Templet/Assets/02.Scripts/TankMove.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Utility;
 
 public class TankMove : MonoBehaviour {
 
@@ -11,18 +12,47 @@ public class TankMove : MonoBehaviour {
     //참조할 컴포넌트를 할당할 변수
     private Rigidbody rbody;
     private Transform tr;
+   
     //키보드 입력값 변수
     private float h, v;
+
+    //PhotonView 컴포넌트를 할당할 변수
+    private PhotonView pv = null;
+
+    //메인 카메라가 추적할 CamPivot 게임오브젝트
+    public Transform camPivot;
+
 	void Start () {
         //컴포넌트 할당
         rbody = GetComponent<Rigidbody>();
         tr = GetComponent<Transform>();
 
         //Rigidbody의 무게중심을 낮게 설정
-        rbody.centerOfMass = new Vector3(0.0f, -0.0f, 0.0f);
+        rbody.centerOfMass = new Vector3(0.0f, -0.5f, 0.0f);
+
+        //PhotonView 컴포넌트 할당
+        pv = GetComponent<PhotonView>();
+
+        //PhotonView가 자신의 탱크일 경우
+        if (pv.isMine)
+        {
+            //메인 카메라에 추가된 SmoothFollow 스크립트에 추적 대상을 연결
+            Camera.main.GetComponent<SmoothFollow>().target = camPivot;
+
+            //Rigidbody의 무게 중심을 낮게 설정
+            rbody.centerOfMass = new Vector3(0.0f, -0.5f, 0.0f);
+        }else
+        {
+            //원격 네트워크 플레이어의 탱크는 물리력을 이용하지 않음
+            rbody.isKinematic = true;
+        }
+
 	}
 	
 	void Update () {
+        //자신이 만든 네트워크 게임오브젝트가 아닌 경우는 키보드 조작 루틴을 나감
+        if (!pv.isMine) return;
+
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
 
